cfec9ad76c906b45a23c6d2e469a42994a5047d0
Eric R. Hoglund <hoglunder@ornl.gov>
2025-01-22 16:42:59 -0500

    Added all major 2D lenses
    Progress in #1
    
    Finished:
    - Quad
    - Drift
    - Lens
    - Prism
    
    Other:
    - Sole is implemented in Lens

--- rayTEM/elements.py
+++ rayTEM/elements.py
@@ -17,8 +17,8 @@ from warnings import warn
 class Element:
     def __init__(self, name:str='Unnamed',
                  kind:None|str=None, poles:None|int=None,
-                 position:float=0, length:float=0,
-                 strength:float=0, calibration:None|float=None,
+                 position:float=0., length:float=0.,
+                 strength:float=0., calibration:None|float=None,
                  ndim:int=2, chroma_dim:bool=False,
                  label:bool=False, print_fancy:bool=True
                  ) -> object:
@@ -211,12 +211,323 @@ class Element:
         rf[...,-2] = s
 
         return rf
+
+
+class Drift(Element):
+    def __init__(self, name:str='', 
+                 position:float=0., length:float=0.,
+                 calibration:None|float=None,
+                 label:bool=False, print_fancy:bool=True) -> object:
+        """Quadripole.
+
+        Parameters
+        ----------
+        name : str, optional
+            Name given to the lens, by default ''
+        position : float, optional
+            The position of the element along the z-axis, by default 0
+        length : int, optional
+            Length of the element, by default 0
+        calibration : float, optional
+            Currnet calibration of the lens in units of ???/A, by default None
+        label : bool, optional
+            If the element should be labeled when plotted, by default False
+        print_fancy : bool, optional
+            If a fancy table should be used when printed, by default True
+        """
         
+        super().__init__(name=name,
+                         kind='Drift', poles=0,
+                         position=position, length=length,
+                         calibration=calibration,
+                         label=label, print_fancy=print_fancy)
+    def transfer_matrix(self,
+                         s:int|float|ArrayLike
+                         #type='Hills' TODO: Add `type` in paramaters to describe the type of transfer matrix. Hill's, Twiss, etc.
+                         ) -> ArrayLike:
+        r"""Transfer matrix for ray propogation.
+        """
+        
+        m = xp.eye(6)
+
+        if self.length != 0:
+            m[0,1] = s
+            m[2,3] = s
+        elif self.length == 0:
+            pass
+
+        return m
+
+class Quadripole(Element):
+    def __init__(self, name:str='', 
+                 position:float=0., length:float=0.,
+                 strength:float=0., calibration:None|float=None,
+                 label:bool=False, print_fancy:bool=True) -> object:
+        """Quadripole.
+
+        Parameters
+        ----------
+        name : str, optional
+            Name given to the lens, by default ''
+        position : float, optional
+            The position of the element along the z-axis, by default 0
+        length : int, optional
+            Length of the element, by default 0
+        strength : float, optional
+            Defined as the focal length, by default 0
+            Note this in not the focusing strength (K) and is simply f.
+            A thin lens is defind as KL=-1/fas L goes to zero.
+        calibration : float, optional
+            Currnet calibration of the lens in units of ???/A, by default None
+        label : bool, optional
+            If the element should be labeled when plotted, by default False
+        print_fancy : bool, optional
+            If a fancy table should be used when printed, by default True
+        """
+        
+        if length == 0: kind = 'Thin quad'
+        else:           kind = 'Quad'
+        super().__init__(name=name,
+                         kind=kind, poles=4,
+                         position=position, length=length, 
+                         strength=strength, calibration=calibration,
+                         label=label, print_fancy=print_fancy)
+    def transfer_matrix(self,
+                         s:int|float|ArrayLike
+                         #type='Hills' TODO: Add `type` in paramaters to describe the type of transfer matrix. Hill's, Twiss, etc.
+                         ) -> ArrayLike:
+        r"""Transfer matrix for ray propogation.
+        
+        The homogenous equaiton of motion approximation leads to a linear solution of $u"+k(s)u=0$ given as $u(s)=C(s)u_0+S(s)u_0', where s is the distance traveled (~z for small u').
+        For K>0 $C=cos(\sqrt{Ks})$ and $S=\frac{1}{\sqrt{K}} sin(\sqrt{Ks})$ and for K<0 $C=cosh(\sqrt{|K|s})$ and $S=\frac{1}{\sqrt{|K|}} sinh(\sqrt{|K|s})$.
+
+        To Do
+        -----
+        """
+        
+        m = xp.eye(6)
+
+        if self.length != 0:
+            sK = xp.sqrt(xp.abs(self.strength))
+            #get trig functions for transfer matrix
+            C = xp.cos(sK*s)
+            S = 1/sK * xp.sin(sK*s)
+            dC = -sK * xp.sin(sK*s)
+            dS = C
+            trig = xp.array([[C ,  S],
+                             [dC, dS]])
+            Ch = xp.cosh(sK*s)
+            Sh = 1/sK * xp.sinh(sK*s)
+            dCh =  sK * xp.sinh(sK*s)
+            dSh = Ch
+            trigh = xp.array([[Ch ,  Sh],
+                              [dCh, dSh]])
+            if self.strength>0: #focusing, trig funcitons
+                m[:2, :2] = trig
+                m[2:4,2:4] = trigh
+            elif self.strength<0: #defocusing, hyperbolic trig functions
+                m[:2, :2] = trigh
+                m[2:4,2:4] = trig
+            else: #drift
+                m = Drift.transfer_matrix(s)
+        elif self.length == 0:
+            f = self.strength
+            if self.strength>0:
+                m[1,0] = -1/f
+                m[3,2] = 1/f
+            elif self.strength>0:
+                m[1,0] = 1/f
+                m[3,2] = -1/f
+            else: #off
+                pass
+
+        return m
+
+class Lens(Element):
+    def __init__(self, name:str='', 
+                 position:float=0., length:float=0.,
+                 strength:float=0., calibration:None|float=None,
+                 label:bool=False, print_fancy:bool=True) -> object:
+        """Quadripole.
+
+        Parameters
+        ----------
+        name : str, optional
+            Name given to the lens, by default ''
+        position : float, optional
+            The position of the element along the z-axis, by default 0
+        length : int, optional
+            Length of the element, by default 0
+        strength : float, optional
+            Defined as the focal length, by default 0
+            Note this in not the focusing strength (K) and is simply f.
+            A thin lens is defind as KL=-1/fas L goes to zero.
+        calibration : float, optional
+            Currnet calibration of the lens in units of ???/A, by default None
+        label : bool, optional
+            If the element should be labeled when plotted, by default False
+        print_fancy : bool, optional
+            If a fancy table should be used when printed, by default True
+        """
+        
+        if length == 0: kind = 'Thin lens'
+        else:           kind = 'QLens'
+        super().__init__(name=name,
+                         kind=kind, poles=None,
+                         position=position, length=length, 
+                         strength=strength, calibration=calibration,
+                         label=label, print_fancy=print_fancy)
+    def transfer_matrix(self,
+                         s:int|float|ArrayLike
+                         #type='Hills' TODO: Add `type` in paramaters to describe the type of transfer matrix. Hill's, Twiss, etc.
+                         ) -> ArrayLike:
+        r"""Transfer matrix for ray propogation.
+
+        To do
+        -----
+        #TODO: Figure out cross terms related to rotation. i.e. m[:2,3:4] and m[3:4,:2]
+        """
+        
+        m = xp.eye(6)
+
+        if self.length != 0:
+            sK = xp.sqrt(xp.abs(self.strength))
+            #get trig functions for transfer matrix
+            C = xp.cos(sK*s)
+            S = 1/sK * xp.sin(sK*s)
+            dC = -sK * xp.sin(sK*s)
+            dS = C
+            trig = xp.array([[C ,  S],
+                             [dC, dS]])
+            if self.strength!=0: #focusing, trig funcitons
+                m[:2, :2] = trig
+                m[2:4,2:4] = trig
+            else: #drift
+                m = Drift.transfer_matrix(s)
+        elif self.length == 0:
+            f = self.strength
+            if self.strength!=0:
+                m[1,0] = -1/f
+                m[3,2] = -1/f
+            else: #off
+                pass
+
+        return m
+
+class Prism(Element):
+    def __init__(self, name:str='', 
+                 position:float=0., length:float=0.,
+                 radius:None|float=None, angle:float=45., w:float=1., g:float=1., k1:float=0.,
+                 strength:float=0., calibration:None|float=None,
+                 label:bool=False, print_fancy:bool=True) -> object:
+        """Prism.
+
+        Parameters
+        ----------
+        name : str, optional
+            Name given to the lens, by default ''
+        position : float, optional
+            The position of the element along the z-axis, by default 0
+        length : int, optional
+            Length of the element, by default 0
+        strength : float, optional
+            Defined as the focal length, by default 0
+            Note this in not the focusing strength (K) and is simply f.
+            A thin lens is defind as KL=-1/fas L goes to zero.
+        calibration : float, optional
+            Currnet calibration of the lens in units of ???/A, by default None
+        label : bool, optional
+            If the element should be labeled when plotted, by default False
+        print_fancy : bool, optional
+            If a fancy table should be used when printed, by default True
+
+        To do
+        -----
+        #TODO: Include dispersion (R16). See Egerton eq 2.14
+        #TODO: Include other spectrometer aberrations. See Egerton eq 2.13
+        """
+
+        self.angle = angle
+        self.angle_rad = xp.deg2rad(angle)
+        if radius is not None and length is not None:
+            raise ValueError('Only specify the length or radius as providing both with radius is not unique.')
+        elif radius is not None:
+            length = angle_rad * radius
+        elif length is not None:
+            radius = length/angle_rad 
+        else:
+            raise ValueError('Either radius or length need to be specified.')
+
+        super().__init__(name=name,
+                         kind='Prism', poles=2,
+                         position=position, length=length,
+                         strength=strength, calibration=calibration,
+                         label=label, print_fancy=print_fancy)
+        self.radius = radius
+        self.w = w
+        self.g = g
+        self.K1 = k1
+
+    def focus_matrix(self,
+                     #type='Hills' TODO: Add `type` in paramaters to describe the type of transfer matrix. Hill's, Twiss, etc.
+                     ) -> ArrayLike:
+        r"""Transfer matrix for the entrance/exit surfaces of the spectrometer used for ray propogation.
+        """
+        m = xp.eye(6)
+
+        if self.strength!=0:
+            m[1,0] = xp.tan(self.strength) / self.radius
+            if k1 == 0:
+                m[1,0] = - xp.tan(self.strength) / self.radius
+            else: #include fringe fields
+                psi = (self.g/self.R) * self.k1 * (1+xp.sin(self.strength)**2)/xp.cos(self.strength)
+                m[2:4,2:4] = - xp.tan(self.strength - psi) / self.radius
+        else: #drif
+            pass
+
+        return m
+    
+    def bending_matrix(self,
+                       s:int|float|ArrayLike,
+                       #type='Hills' TODO: Add `type` in paramaters to describe the type of transfer matrix. Hill's, Twiss, etc.
+                       ) -> ArrayLike:
+        r"""Transfer matrix for the bending of the spectrometer used for ray propogation.
+        """
+        m = xp.eye(6)
+        u = s/self.radius
+
+        if self.strength!=0:
+            m[0,0] = xp.cos(u)
+            m[1,1] = xp.cos(u)
+            m[0,1] = self.radius * xp.sin(u)
+            m[1,0] = - xp.sin(u)/self.radius
+        else:
+            m = Drift.transfer_matrix(s)
+
+        return m
+        
+    def transfer_matrix(self,
+                         s:int|float|ArrayLike,
+                         #type='Hills' TODO: Add `type` in paramaters to describe the type of transfer matrix. Hill's, Twiss, etc.
+                         ) -> ArrayLike:
+        r"""Transfer matrix for ray propogation.
+        """
+        
+        m_focus1 = self.focus_matrix()
+        m_bend   = self.bending_matrix(s)
+        m_focus2 = self.focus_matrix()
+
+        m = m_focus2 @ m_bend @ m_focus1
+
+        return m
+
+
+
 class Element1D(Element):
     def __init__(self, name:str='Unnamed',
                  kind:None|str=None, poles:None|int=None,
-                 position:float=0, length:float=0,
-                 strength:float=0, calibration:None|float=None,
+                 position:float=0., length:float=0.,
+                 strength:float=0., calibration:None|float=None,
                  label:bool=False, print_fancy:bool=True
                  ) -> object:
         """
@@ -232,9 +543,9 @@ class Element1D(Element):
 
 class Quadripole1D(Element1D):
     def __init__(self, name:str='', 
-                 position:float=0, length:float=0,
-                 strength:float=0, calibration:float=None,
-                 label:float=False, print_fancy:float=True) -> object:
+                 position:float=0., length:float=0.,
+                 strength:float=0., calibration:None|float=None,
+                 label:bool=False, print_fancy:bool=True) -> object:
         """1D Quadripole. This effectively acts as a lens in 1D.
 
         Parameters
@@ -324,9 +635,9 @@ class Quadripole1D(Element1D):
 
 class Lens1D(Element1D):
     def __init__(self, name:str='', 
-                 position:float=0, length:float=0,
-                 strength:float=0, calibration:float=None,
-                 label:float=False, print_fancy:float=True) -> object:
+                 position:float=0., length:float=0.,
+                 strength:float=0., calibration:None|float=None,
+                 label:bool=False, print_fancy:bool=True) -> object:
         """1D round lens.
 
         Parameters
