d9e4bc814db4f5cf5fa35f5957ee1445d1945741
tpchuckles <tpchuckles@gmail.com>
2025-11-19 15:41:29 -0500

    add AS2 querier

---
+++ pySEA/rayTEM/AS2.py
@@ -0,0 +1,21 @@
+import requests
+import numpy as np
+
+class AS2querier:
+	def __init__(self,ip,port,quiet=False):
+		self.url = "http://"+str(ip)+":"+str(port)+"/AS2/controls/"
+		self.quiet = quiet
+	def query(self,element_name):
+		try:
+			r = requests.get(self.url+element_name)
+			s = r.status_code
+		except:
+			s = 0
+		if 0 != 200:
+			if not self.quiet:
+				print("WARNING: failed response from",self.url,"for element",element_name,"(returning random)")
+			return np.random.random()
+		v = r.json()["OutputValue"]
+		if not self.quiet:
+			print("queried:",element_name,"=",v)
+		return v
